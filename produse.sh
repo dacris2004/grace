7a8230d115edeeb702b14df677625107f1a90dde
dacris2004 <dacris2004@yahoo.com>
2024-07-07 16:31:33 +0300

    Create produse.sh
    Scriptul gestioneaza produsele.  Le afiseaza sub forma de meniu navigabil, un produs se sterge cu d si se adauga cu a. Se iese cu q.

---
+++ produse.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# Numele fisierului care contine lista de produse si preturi
+FILE="produse.txt"
+
+# Functie pentru a citi produsele si preturile din fisier
+read_products() {
+    mapfile -t products < "$FILE"
+    for i in "${!products[@]}"; do
+        products[i]=$(echo "${products[i]}" | tr -d '\r')
+    done
+}
+
+# Functie pentru a afisa meniul produselor
+display_products() {
+    clear
+    echo "=== Lista de produse ==="
+    local columns=$(tput cols)
+    local column_width=40
+    local num_columns=$((columns / column_width))
+    local num_rows=$(( (${#products[@]} + num_columns - 1) / num_columns ))
+
+    for row in $(seq 0 $((num_rows - 1))); do
+        for col in $(seq 0 $((num_columns - 1))); do
+            index=$((row + col * num_rows))
+            if [[ $index -lt ${#products[@]} ]]; then
+                if [[ $index -eq $current_index ]]; then
+                    # Evidentiem produsul selectat cu culori inversate
+                    printf "\e[7m%02d. %-35s\e[0m" "$((index+1))" "${products[$index]}"
+                else
+                    printf "%02d. %-35s" "$((index+1))" "${products[$index]}"
+                fi
+            fi
+        done
+        echo ""
+    done
+
+    echo "========================================================="
+    echo "Apasati 'a' pentru a adauga un produs, 'd' pentru a sterge un produs, 'q' pentru a iesi."
+}
+
+# Functie pentru a adauga un produs nou
+add_product() {
+    echo -n "Introduceti numele produsului: "
+    read -r product_name
+    echo -n "Introduceti pretul produsului: "
+    read -r product_price
+
+    if [[ -n "$product_name" && -n "$product_price" ]]; then
+        echo "$product_name:$product_price RON" >> "$FILE"
+        read_products
+        display_products
+    else
+        echo "Nume sau pret invalid. Produsul nu a fost adaugat."
+    fi
+}
+
+# Functie pentru a sterge un produs
+delete_product() {
+    local index=$current_index
+    unset 'products[index]'
+    printf "%s\n" "${products[@]}" > "$FILE"
+    read_products
+}
+
+# Initializam indexul curent
+current_index=0
+
+# Citim produsele din fisier
+read_products
+
+# Afisam meniul initial
+display_products
+
+# Bucla principala pentru a gestiona intrarile utilizatorului
+while true; do
+    read -rsn1 input
+    case "$input" in
+        $'\x1b') # Detectam tasta Esc
+            read -rsn2 -t 0.1 input
+            if [[ "$input" == "[A" ]]; then
+                # Sageata sus
+                ((current_index--))
+                if [[ $current_index -lt 0 ]]; then
+                    current_index=$(( ${#products[@]} - 1 ))
+                fi
+            elif [[ "$input" == "[B" ]]; then
+                # Sageata jos
+                ((current_index++))
+                if [[ $current_index -ge ${#products[@]} ]]; then
+                    current_index=0
+                fi
+            fi
+            ;;
+        [0-9]) # Detectam o cifra, pregatim sa citim a doua cifra
+            read -rsn1 input2
+            if [[ "$input2" =~ [0-9] ]]; then
+                input+="$input2"
+                if [[ "$input" =~ ^[0-9]{2}$ ]]; then
+                    index=$((10#"$input" - 1))
+                    if [[ $index -ge 0 && $index -lt ${#products[@]} ]]; then
+                        current_index=$index
+                    fi
+                fi
+            fi
+            ;;
+        a)
+            add_product
+            ;;
+        d)
+            delete_product
+            ;;
+        q)
+            echo "Iesire..."
+            break
+            ;;
+        *)
+            echo "Optiune invalida!"
+            ;;
+    esac
+    display_products
+done
+
+clear
+echo "Program inchis."
